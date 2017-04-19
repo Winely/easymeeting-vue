1f1cf3b8bc899eb28d511f5ee4372f958ab1c2d7
dongg <dongguho@gmail.com>
2017-04-19 23:13:36 +0800

    修改了radios的样式；调整了一点global

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -6,10 +6,13 @@
       <input name="userPassword1" type="password" placeholder="密码">
       <input name="userPassword2" type="password" placeholder="确认密码">
       <div class="radio-form">
-        <input id="male" class="gender-radio" type="radio" name="gender" value="1">
-        <label for="male" class="gender-label">男</label>
-        <input id="female" class="gender-radio" type="radio" name="gender" value="0">
-        <label for="female" class="gender-label">女</label>
+        <input id="male" type="radio" name="gender" value="1">
+        <label for="male"></label>
+        <label class="gender-label" for="male">男</label>
+
+        <input id="female" type="radio" name="gender" value="0">
+        <label for="female"></label>
+        <label class="gender-label" for="female">女</label>
       </div>
       <textarea placeholder="自我介绍"></textarea>
       <button class="submit-button" type="submit">注 册</button>
@@ -22,40 +25,19 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  input {
-    border-radius: 18px;
+  theme-color=#8ab537
+  bg-color=#F1F4E4
+  input, textarea {
+    border-radius: 8px;
     height: 28px;
     width: 200px;
     margin: 6px;
     padding: 2px 18px;
   }
 
-  input[type=radio], label {
-    width: 30px;
-    height: 20px;
-    margin: 6px;
-    padding: 0;
-    font-size: 15px;
-  }
-
-  .gender-label {
-    margin: 0;
-    margin-right: 30px;
-    vertical-align: middle;
-  }
-
-  .gender-radio {
-    vertical-align: middle;
-  }
-
-  textarea {
-    width: 238px;
-    height: 150px;
-    border-radius: 5px;
-    padding: 5px 8px;
-    margin-top: 5px;
-    resize: none;
-  }
+  textarea
+    height 150px
+    padding-top 8px
 
   .signPageWrap {
     width: 100%;
@@ -68,10 +50,34 @@
     height: 100%;
     width: 30%;
     min-width: 300px;
-    background-color: rgb(241, 244, 228);
+    background-color: bg-color;
     display: flex
     align-items: center;
     justify-content: center;
     flex-direction: column;
   }
+
+  .radio-form
+    display flex
+    align-items center
+    justify-content space-around
+    margin 10px auto
+    input
+      position absolute
+      left -999px
+    input + label
+      display inline-block
+      width 14px
+      height 14px
+      background transparent
+      border solid 2px theme-color
+      border-radius 4px
+      margin-right 18px
+      margin-left 20px
+      transition-duration .3s
+    input:checked + label
+      background theme-color
+    .gender-label
+      margin-right 40px
+
 </style>

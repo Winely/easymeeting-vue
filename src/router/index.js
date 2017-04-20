0aba4d64a6c8cdfa4bef959543f178f29afe92d4
dongg <dongguho@gmail.com>
2017-04-20 19:27:39 +0800

    设置了默认路由

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,13 @@ import Setting from 'components/settingview'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
     { path: '/group', component: Group },
     { path: '/calendar', component: Calendar },
     { path: '/setting', component: Setting }
   ]
 })
+
+router.replace('/group')
+export default router

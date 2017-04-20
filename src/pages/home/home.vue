b49868ec1cfaef40469adb017217555e67f37724
dongg <dongguho@gmail.com>
2017-04-20 19:43:36 +0800

    bug fixed

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -11,13 +11,11 @@ cd .
 <script>
   require('../../assets/global.css')
   import header from 'components/header'
-  import footer from 'components/footer'
   import sidebar from 'components/sidebar'
   export default {
     name: 'app',
     components: {
       'v-header': header,
-      'v-footer': footer,
       sidebar
     },
     data () {

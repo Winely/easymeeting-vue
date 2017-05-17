8e85a132a6967b2b60a1b17b13df4cb2e33dd86f
dongg <dongguho@gmail.com>
2017-05-17 08:33:33 +0800

    add meeting page

---
+++ src/pages/meeting/meeting.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <v-header :user="user" @logout="logout"></v-header>
+  </div>
+</template>
+
+<script>
+  require('../../assets/global.css')
+  import urlconf from 'assets/url.conf'
+  import header from 'components/header'
+
+  export default {
+    name: 'app',
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>

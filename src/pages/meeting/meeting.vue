7a4940cb57990add8fff82832cb7bbdc2cd1ff8a
dongg <dongguho@gmail.com>
2017-05-17 09:38:39 +0800

    跑了一个轮砸

--- src/pages/meeting/meeting.vue
+++ src/pages/meeting/meeting.vue
@@ -1,21 +1,34 @@
 <template>
   <div id="app">
-    <v-header :user="user" @logout="logout"></v-header>
+    <div id="docpad"></div>
   </div>
 </template>
 
 <script>
   require('../../assets/global.css')
+  require('../../assets/ether-pad')
   import urlconf from 'assets/url.conf'
-  import header from 'components/header'
 
   export default {
     name: 'app',
-    components: {
-      'v-header': header
+    components: {},
+    mounted () {
+      $('#docpad').pad({'padId': 'test','showChat':true, 'plugins':{'pageview':'true'}})
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width: 90vw
+    height: 90vh
+
+  #docpad
+    width 100%
+    height 100%
+    box-shadow 0 3px 20px #eee
+
+  #epframedocpad
+    height: 100%
+    width: 100%
 </style>
